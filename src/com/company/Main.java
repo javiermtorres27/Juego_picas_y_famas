38aba7c1f2799b894e5f36ab94525d76ebd5fe5c
Javier M. Torres <javiermtorres27@gmail.com>
2022-04-28 15:25:35 -0500

    se gregó menú principal

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Menu.mostrarMenu();
+
     }
 }
--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Menu {
+package com.company;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    public static void mostrarMenu() {
+        System.out.println("---------------------");
+        System.out.println("Bienvenido al famoso juego Picas y Famas.\nSelecciona una opción:");
+
+        int respuesta = 3;
+
+        do {
+            System.out.println("1. Ingresar/Eliminar Jugadores.");
+            System.out.println("2. Jugar una partida Rápida.");
+            System.out.println("3. Salir del juego.");
+
+            Scanner sc = new Scanner(System.in);
+            respuesta = Integer.valueOf(sc.nextLine());
+
+            switch (respuesta) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    System.out.println("Hasta la próxima.");
+                    break;
+                default:
+                    System.out.println("---------------------");
+                    System.out.println("Por favor, selecciona una opción.");
+            }
+        } while (respuesta != 3);
+    }
+
 }
